d7c2b87eff810231547a6151bf191aad2432c23c
Amin Husni <aminhusni@users.noreply.github.com>
2019-02-09 04:11:53 +0800

    Get Actual Requester's IP
    Since the program is running behind an Nginx proxy, the requester's IP  has been corrected by using "X-Forwarded-For" and "X-Real-Ip" headers.

--- handler/hls.go
+++ handler/hls.go
@@ -27,6 +27,8 @@ import (
     "crypto/rand"
     "encoding/base64"
     "log"
+    "net"
+    "bytes"
 
     "github.com/faryon93/hlswatch/state"
 )
@@ -40,6 +42,84 @@ const (
     PLAYLIST_FILE_EXTENSION = "m3u8"
     TOKEN_URL_PARAMETER     = "token"
 )
+
+//ipRange - a structure that holds the start and end of a range of ip addresses
+type ipRange struct {
+    start net.IP
+    end net.IP
+}
+
+// inRange - check to see if a given ip address is within a range given
+func inRange(r ipRange, ipAddress net.IP) bool {
+    // strcmp type byte comparison
+    if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
+        return true
+    }
+    return false
+}
+
+var privateRanges = []ipRange{
+    ipRange{
+        start: net.ParseIP("10.0.0.0"),
+        end:   net.ParseIP("10.255.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("100.64.0.0"),
+        end:   net.ParseIP("100.127.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("172.16.0.0"),
+        end:   net.ParseIP("172.31.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("192.0.0.0"),
+        end:   net.ParseIP("192.0.0.255"),
+    },
+    ipRange{
+        start: net.ParseIP("192.168.0.0"),
+        end:   net.ParseIP("192.168.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("198.18.0.0"),
+        end:   net.ParseIP("198.19.255.255"),
+    },
+}
+
+
+// isPrivateSubnet - check to see if this ip is in a private subnet
+func isPrivateSubnet(ipAddress net.IP) bool {
+    // my use case is only concerned with ipv4 atm
+    if ipCheck := ipAddress.To4(); ipCheck != nil {
+        // iterate over all our ranges
+        for _, r := range privateRanges {
+            // check if this ip is in a private range
+            if inRange(r, ipAddress){
+                return true
+            }
+        }
+    }
+    return false
+}
+
+
+func getIPAdress(r *http.Request) string {
+    for _, h := range []string{"X-Forwarded-For", "X-Real-Ip"} {
+        addresses := strings.Split(r.Header.Get(h), ",")
+        // march from right to left until we get a public address
+        // that will be the address right before our proxy.
+        for i := len(addresses) -1 ; i >= 0; i-- {
+            ip := strings.TrimSpace(addresses[i])
+            // header can contain spaces too, strip those out.
+            realIP := net.ParseIP(ip)
+            if !realIP.IsGlobalUnicast() || isPrivateSubnet(realIP) {
+                // bad address, go to next
+                continue
+            }
+            return ip
+        }
+    }
+    return ""
+}
 
 
 // --------------------------------------------------------------------------------------
@@ -84,12 +164,14 @@ func Hls(ctx *state.State, h http.Handler) http.Handler {
             // generate a new token if the client does not supply one
             if token == "" {
                 token = nextToken()
-
+                
+                actualIP := getIPAdress(r)
+                
                 stream.Lock()
                 stream.Viewers[token] = &state.Viewer{
                     FirstSeen: time.Now(),
                     LastSeen: time.Now(),
-                    Ip: r.RemoteAddr,
+                    Ip: actualIP,
                 }
                 stream.Unlock()
 
